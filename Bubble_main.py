f99ad339c9c68f6ff57cfaa361cfd43e47bb769c
tomsalden <tomsalden@live.nl>
2020-07-09 15:22:45 +0200

    Create function to omit maximum servo values

--- Bubble_main.py
+++ Bubble_main.py
@@ -1,14 +1,22 @@
+# Software for the Bubble robot
+# This software allows the robot to move its head
+# to random positions. For now, the software is not able to interact
+# or see the world, but a camera with detection will be added to the software.
+
+# General import statements
 import pigpio
 from time import sleep
 import random
+from omitMaxValue import *
 
-# connect to the
+# Initialisations
 pi = pigpio.pi()
 yawServo = 17     #Servo op z'n hoofd
 pitchServo = 22   #Servo voor schuinkijken
 rollServo = 27    #Servo voor hoofddraaien
 
-midden = 1500
+# Initial values for the variables
+middle = 1500
 yawMin = 1000
 yawMax = 2000
 pitchMin = 1200
@@ -16,45 +24,35 @@ pitchMax = 1900
 rollMin = 1100
 rollMax = 1900
 
-pi.set_servo_pulsewidth(yawServo, midden)
-pi.set_servo_pulsewidth(pitchServo, midden)
-pi.set_servo_pulsewidth(rollServo, midden)
+# Set the servos to their middle values
+pi.set_servo_pulsewidth(yawServo, middle)
+pi.set_servo_pulsewidth(pitchServo, middle)
+pi.set_servo_pulsewidth(rollServo, middle)
 
-yawPositie = 1500
-pitchPositie = 1500
-rollPositie = 1500
+yawPositie = middle
+pitchPositie = middle
+rollPositie = middle
 
 
+# Program loop
 while True:
+    # Get the random integers for rotating the head
     draaitijd = random.randint(20,51)
     yawStappen = random.randint(-1000,1000)
     pitchStappen = random.randint(-800,800)
     rollStappen = random.randint(-800,800)
 
-    if yawPositie + yawStappen > yawMax:
-        yawStappen = yawMax-yawPositie
-    if yawPositie + yawStappen < yawMin:
-        yawStappen = yawMin-yawPositie
+    # Make sure the maximum potmeter valus are not exceeded
+    yawStappen = omitMaxValue(yawPositie,yawStappen,yawMax,yawMax)
+    pitchStappen = omitMaxValue(pitchPositie,pitchStappen,pitchMax,pitchMax)
+    rollStappen = omitMaxValue(rollPositie,rollStappen,rollMax,rollMax)
 
-    if pitchPositie + pitchStappen > pitchMax:
-        pitchStappen = pitchMax-pitchPositie
-    if pitchPositie + pitchStappen < pitchMin:
-        pitchStappen = pitchMin-pitchPositie
-
-    if rollPositie + rollStappen > rollMax:
-        rollStappen = rollMax-rollPositie
-    if rollPositie + rollStappen < rollMin:
-        rollStappen = rollMin-rollPositie
-
+    # Subdivide the steps to set the steps/time for the servos
     yawOnderverdeeld = int(float(yawStappen)/draaitijd)
     pitchOnderverdeeld = int(float(pitchStappen)/draaitijd)
     rollOnderverdeeld = int(float(rollStappen)/draaitijd)
 
-    #print('draaitijd', draaitijd)
-    #print('yaw',yawPositie + yawStappen, yawStappen , yawMax, yawMin)
-    #print('pitch',pitchPositie + pitchStappen, pitchPositie, pitchStappen, pitchOnderverdeeld, pitchMax, pitchMin)
-    #print('roll',rollPositie + rollStappen, rollStappen, rollMax, rollMin)
-
+    # Run the servo sequence
     for x in range(0,draaitijd):
         yawPositie = yawPositie + yawOnderverdeeld
         pitchPositie = pitchPositie + pitchOnderverdeeld
@@ -66,6 +64,7 @@ while True:
 
         sleep(0.05)
 
+    # Turn off the servos
     pi.set_servo_pulsewidth(yawServo, 0)
     pi.set_servo_pulsewidth(pitchServo, 0)
     pi.set_servo_pulsewidth(rollServo, 0)
---
+++ omitMaxValue.py
@@ -0,0 +1,10 @@
+# omitMaxValue.py
+# Make sure the servo will not pass the maximum allowed values
+
+def omitMaxValue(currentPos, stepsToTake, maxPos, minPos):
+    if currentPos + stepsToTake > maxPos:
+        stepsToTake = maxPos-currentPos
+    if currentPos + stepsToTake < minPos:
+        stepsToTake = minPos-currentPos
+
+        return stepsToTake
