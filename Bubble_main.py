a09ad5d711344f6c763fd006b78ce8c1c546952d
tomsalden <tomsalden@live.nl>
2020-07-10 09:36:59 +0200

    Created config.py for global variables

--- Bubble_main.py
+++ Bubble_main.py
@@ -7,16 +7,10 @@
 import pigpio
 from time import sleep
 import random
-from omitMaxValue import *
+import initialisation
+import omitMaxValue
+import moveServos
 
-# Initialisations
-pi = pigpio.pi()
-yawServo = 17     #Servo op z'n hoofd
-pitchServo = 22   #Servo voor schuinkijken
-rollServo = 27    #Servo voor hoofddraaien
-
-# Initial values for the variables
-middle = 1500
 yawMin = 1000
 yawMax = 2000
 pitchMin = 1200
@@ -25,51 +19,47 @@ rollMin = 1100
 rollMax = 1900
 
 # Set the servos to their middle values
-pi.set_servo_pulsewidth(yawServo, middle)
-pi.set_servo_pulsewidth(pitchServo, middle)
-pi.set_servo_pulsewidth(rollServo, middle)
-
-yawPositie = middle
-pitchPositie = middle
-rollPositie = middle
+config.pi.set_servo_pulsewidth(config.yawServo, middle)
+config.pi.set_servo_pulsewidth(config.pitchServo, middle)
+config.pi.set_servo_pulsewidth(config.rollServo, middle)
 
 
 # Program loop
 while True:
     # Get the random integers for rotating the head
-    draaitijd = random.randint(20,51)
-    yawStappen = random.randint(-1000,1000)
-    pitchStappen = random.randint(-800,800)
-    rollStappen = random.randint(-800,800)
+    config.totalSteps = random.randint(20,51)
+    config.yawSteps = random.randint(-1000,1000)
+    config.pitchSteps = random.randint(-800,800)
+    config.rollSteps = random.randint(-800,800)
 
 
     # Make sure the maximum potmeter valus are not exceeded
-    yawStappen = omitMaxValue(yawPositie,yawStappen,yawMin,yawMax)
-    pitchStappen = omitMaxValue(pitchPositie,pitchStappen,pitchMin,pitchMax)
-    rollStappen = omitMaxValue(rollPositie,rollStappen,rollMin,rollMax)
+    config.yawSteps = omitMaxValue.MaxValue(config.yawPosition,config.yawSteps,yawMin,yawMax)
+    config.pitchSteps = omitMaxValue.MaxValue(config.pitchPosition,config.pitchSteps,pitchMin,pitchMax)
+    config.rollSteps = omitMaxValue.MaxValue(config.rollPosition,config.rollSteps,rollMin,rollMax)
 
 
     # Subdivide the steps to set the steps/time for the servos
-    yawOnderverdeeld = int(float(yawStappen)/draaitijd)
-    pitchOnderverdeeld = int(float(pitchStappen)/draaitijd)
-    rollOnderverdeeld = int(float(rollStappen)/draaitijd)
+    yawDivided = int(float(config.yawSteps)/config.totalSteps)
+    pitchDivided = int(float(config.pitchSteps)/config.totalSteps)
+    rollDivided = int(float(config.rollSteps)/config.totalSteps)
 
     # Run the servo sequence
-    for x in range(0,draaitijd):
-        yawPositie = yawPositie + yawOnderverdeeld
-        pitchPositie = pitchPositie + pitchOnderverdeeld
-        rollPositie = rollPositie + rollOnderverdeeld
+    for x in range(0,config.totalSteps):
+        config.yawPosition = config.yawPosition + yawDivided
+        config.pitchPosition = config.pitchPosition + pitchDivided
+        config.rollPosition = config.rollPosition + rollDivided
 
-        pi.set_servo_pulsewidth(yawServo, yawPositie)
-        pi.set_servo_pulsewidth(pitchServo, pitchPositie)
-        pi.set_servo_pulsewidth(rollServo, rollPositie)
+        config.pi.set_servo_pulsewidth(config.yawServo, config.yawPosition)
+        config.pi.set_servo_pulsewidth(config.pitchServo, config.pitchPosition)
+        config.pi.set_servo_pulsewidth(config.rollServo, config.rollPosition)
 
         sleep(0.04)
 
     # Turn off the servos
-    pi.set_servo_pulsewidth(yawServo, 0)
-    pi.set_servo_pulsewidth(pitchServo, 0)
-    pi.set_servo_pulsewidth(rollServo, 0)
+    pi.set_servo_pulsewidth(config.yawServo, 0)
+    pi.set_servo_pulsewidth(config.pitchServo, 0)
+    pi.set_servo_pulsewidth(config.rollServo, 0)
 
     #sleep(1)
     #test erbij
---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py>
+
+# Define global variables
+
+# Initialisations
+pi = pigpio.pi()
+yawServo = 17     #Servo op z'n hoofd
+pitchServo = 22   #Servo voor schuinkijken
+rollServo = 27    #Servo voor hoofddraaien
+
+# Initial values for the variables
+middle = 1500
+
+yawPosition = middle
+pitchPosition = middle
+rollPosition = middle
+
+totalSteps = 20
+yawSteps = 100
+pitchSteps = 100
+rollSteps = 100
