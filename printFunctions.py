56c247eaa9a727f785a16f32de4085e8dcab8be9
tomsalden <tomsalden@live.nl>
2020-07-21 14:42:47 +0200

    Print extra line before face

--- printFunctions.py
+++ printFunctions.py
@@ -8,4 +8,5 @@ def printHeadPosition(headPosition):
     filename = str(str(headPosition[0])+str(headPosition[1])+str(headPosition[2])+".txt")
     f = open("Visuals/" + filename,'r')
     file_contents = f.read()
+    print("\n")
     print(file_contents)
