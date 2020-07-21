fdeba1b2d8f1d4433ced52ebb613d08644c38c19
tomsalden <tomsalden@live.nl>
2020-07-21 11:52:48 +0200

    Added random location picker

--- determineNewPosition.py
+++ determineNewPosition.py
@@ -1,8 +1,17 @@
 # A function to determine the new position of the servos
+# Arguments: opt1 = first position
+#            opt2 = middle positions
+#            opt3 = last positions
+#            x = chosen position
+import random
+
 def newPosMaker(opt1,opt2,opt3,x):
     switcher = {
         0:opt1,
         1:opt2,
         2:opt3
     }
-    return switcher.get(x,opt2)
+    newPositionArea = switcher.get(x,opt2)
+    newPosition = random.randint(newPositionArea[0],newPositionArea[1])
+
+    return newPosition
