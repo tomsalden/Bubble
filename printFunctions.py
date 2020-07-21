646ea2c3835b7c1740d96ca4f06f1a6589ef1817
tomsalden <tomsalden@live.nl>
2020-07-21 14:41:27 +0200

    Fixed error in print function

--- printFunctions.py
+++ printFunctions.py
@@ -6,6 +6,6 @@ def clearPrints():
 
 def printHeadPosition(headPosition):
     filename = str(str(headPosition[0])+str(headPosition[1])+str(headPosition[2])+".txt")
-    f = open("Visuals/" + filestring,'r')
+    f = open("Visuals/" + filename,'r')
     file_contents = f.read()
     print(file_contents)
