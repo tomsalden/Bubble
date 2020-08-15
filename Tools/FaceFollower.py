c8d620c1511ad14daf24274d2f07b1cfce7c4bff
tomsalden <tomsalden@live.nl>
2020-08-15 22:31:26 +0200

    Larger interval2

--- Tools/FaceFollower.py
+++ Tools/FaceFollower.py
@@ -36,7 +36,7 @@ pi.set_servo_pulsewidth(yawServo, yawPosition)
 pi.set_servo_pulsewidth(pitchServo, pitchPosition)
 pi.set_servo_pulsewidth(rollServo, rollPosition)
 
-DistanceSubject = 140 #Estimated 157, tuned to this value
+DistanceSubject = 130 #Estimated 157, tuned to this value
 
 
 #Due to fov of camera and resolution:
